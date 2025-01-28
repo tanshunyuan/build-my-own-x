c3de513665f243806239a783174a28f4a69a95b1
Shun Yuan <tanshunyuan99@gmail.com>
2025-01-28 11:29:49 +0800

    chore: complete pt2

--- crdt/intro-to-crdt/ts/part-2-algebra-and-contracts.ts
+++ crdt/intro-to-crdt/ts/part-2-algebra-and-contracts.ts
@@ -1,7 +1,9 @@
 // https://lars.hupel.info/topics/crdt/02-contracts/
-import fc, { IPropertyWithHooks } from 'fast-check'
+import fc, { Arbitrary, IPropertyWithHooks, sample } from 'fast-check'
 import { assert } from 'chai'
 import { checkAll } from '../misc/fc-runner.js';
+
+const cl = (...items: any) => console.log(...items)
 
 // <= means less than or equal to
 
@@ -27,7 +29,7 @@ const anyComparator: Comparator = {
   isLeq: (x: any, y: any) => x <= y
 }
 
-// partial ordering. To achieve partial ordering, the following laws must be met
+// partial ordering (contract). To achieve partial ordering, the following laws (constraints) must be met
 // - reflexitivity: any value must be <= to itself
 // - transtivity: given any three value: x,y and z. z <= y, y <= z and x <= y
 // - antisymettry 
@@ -52,14 +54,58 @@ contracts.partialOrdering = (instance, gen) => ({
 
 type Ordering = {
   any: Comparator
+  set: Comparator<Set<number | string>>
 }
 const orderings: Ordering = {
   any: {
     isLeq: (x, y) => x <= y
+  },
+  set: {
+    isLeq: (x, y) => x.isSubsetOf(y)
   }
 }
 
+// Promise.all([
+//   checkAll(contracts.partialOrdering(orderings.any, fc.integer())),
+//   checkAll(contracts.partialOrdering(orderings.any, fc.string())),
+// ])
+
+// for implementation we can use chai's deepEqual to ensure both Sets are equal
+assert.deepEqual(new Set([1, 2]), new Set([1, 2]));
+
+// defining a '<=' equivalent for Set
+declare global {
+  interface Set<T> {
+    isSubsetOf(rOperand: Set<T>): boolean
+  }
+}
+
+Object.defineProperty(Set.prototype, 'isSubsetOf', {
+  configurable: true,
+  value: function (rOperand) {
+    const lOperand = this.values()
+    for (const element of lOperand) {
+      if (!rOperand.has(element)) {
+        return false
+      }
+    }
+    return true
+  }
+})
+
+assert.ok(new Set([1, 2]).isSubsetOf(new Set([1, 2, 3])));
+assert.notOk(new Set([1, 3]).isSubsetOf(new Set([1, 2])));
+
+/**
+ * @returns Array of Sets with elements passed in from the generator
+ */
+const smallSetGen = (gen: Arbitrary<number | string>) => fc.array(gen, { maxLength: 5 }).map(elements => {
+  // { elements: [ -154212869, 123330876 ] }
+  return new Set(elements)
+})
+
+
 Promise.all([
-  checkAll(contracts.partialOrdering(orderings.any, fc.integer())),
-  checkAll(contracts.partialOrdering(orderings.any, fc.string())),
+  checkAll(contracts.partialOrdering(orderings.set, smallSetGen(fc.integer()))),
+  checkAll(contracts.partialOrdering(orderings.set, smallSetGen(fc.hexaString({ maxLength: 3 }))))
 ])
