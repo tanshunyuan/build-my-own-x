8c57c01b9d9869593600671480ab3e859bc50049
Shun Yuan <tanshunyuan99@gmail.com>
2025-02-21 22:04:42 +0800

    chore: add more types

--- jsx-stuff/build-your-own-react/v3/src/didact/hooks.ts
+++ jsx-stuff/build-your-own-react/v3/src/didact/hooks.ts
@@ -1,40 +1,54 @@
 import { cl } from "../helper"
 
-// hooks are just a queue, they're comparing their old vs new
-export const useState = <Type>(initial: Type) => {
-  cl(`useState.gWipFiber ==> `, gWipFiber)
-  const oldHook =
-    gWipFiber.alternate &&
-    gWipFiber.alternate.hooks &&
-    gWipFiber.alternate.hooks[hookIndex]
+type SetStateAction<S> = S | ((prevState: S) => S);
+// this technically does accept a second argument, but it's already under a deprecation warning
+// and it's not even released so probably better to not define it.
+type Dispatch<A> = (value: A) => void;
 
-  const hook = {
+interface Hook<S> {
+  state: S;
+  queue: SetStateAction<S>[];
+}
+
+// hooks are just a queue, they're comparing their old vs new
+export const useState = <S = undefined>(initial: S | (() => S)): [S, Dispatch<SetStateAction<S>>] => {
+  cl(`useState.gWipFiber ==> `, window.gWipFiber)
+  const oldHook =
+    window.gWipFiber &&
+    window.hookIndex &&
+    window.gWipFiber.alternate &&
+    window.gWipFiber.alternate.hooks &&
+    window.gWipFiber.alternate.hooks[window.hookIndex]
+
+  const hook: Hook<S> = {
     // if the previous has a state we use it,
     // else we initialse a new one
-    state: oldHook ? oldHook.state : initial,
+    state: oldHook ? oldHook.state : (typeof initial === 'function' ? (initial as () => S)() : initial),
     // stores the action that's going to be applied
     queue: []
   }
   cl(`useState.hook ==> `, hook)
 
-  const actions = oldHook ? oldHook.queue : []
-  actions.forEach(action => {
-    hook.state = action(hook.state)
+  const actions = (oldHook ? oldHook.queue : [])
+  actions.forEach((action: Dispatch<SetStateAction<S>>) => {
+    hook.state = typeof action === 'function' 
+      ? (action as (prevState: S) => S)(hook.state)
+      : action;
   })
 
-  const setState = action => {
+  const setState: Dispatch<SetStateAction<S>> = (action: SetStateAction<S>) => {
     hook.queue.push(action)
-    wipRoot = {
-      dom: currentRoot.dom,
-      props: currentRoot.props,
-      alternate: currentRoot
+    window.wipRoot = {
+      dom: window.currentRoot.dom,
+      props: window.currentRoot.props,
+      alternate: window.currentRoot
     }
-    nextUnitOfWork = wipRoot
-    deletions = []
+    window.nextUnitOfWork = window.wipRoot
+    window.deletions = []
   }
 
-  gWipFiber.hooks.push(hook)
-  hookIndex++
+  window.gWipFiber && window.gWipFiber.hooks && window.gWipFiber.hooks.push(hook)
+  window.hookIndex && window.hookIndex++
 
   return [hook.state, setState]
 
